a890a1677049cc060427e2021eb9143bb41fe9a6
Kirill <kirillmatuzko21@gmail.com>
2026-02-16 15:33:18 +0300

    add about and upd layout

---
+++ resources/views/about.blade.php
@@ -0,0 +1,16 @@
+@extends('layout')
+
+@section('title')
+    Страница о нас
+@endsection
+
+@section('main_content')
+    <div class="jumbotron bg-warning p-4">
+        <h1>Страница о нас</h1>
+        <p class="lead">
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Sint sit reiciendis fugiat rem expedita atque laborum,
+            totam animi inventore dolore tempora iusto voluptatum praesentium nihil esse quae odio reprehenderit placeat!
+        </p>
+        <a href="/review" class="btn btn-lg btn-danger" role="button">Отзывы</a>
+    </div>
+@endsection
--- resources/views/layout.blade.php
+++ resources/views/layout.blade.php
@@ -5,7 +5,7 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <title>Главная страница</title>
+    <title>@yield('title')</title>
     <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css">
 </head>
 
@@ -22,7 +22,7 @@
             </div>
         </header>
     </div>
-    <div class="container">
+    <div class="container mt-5">
         @yield('main_content')
     </div>
 
