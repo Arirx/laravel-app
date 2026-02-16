cd7fa5300c5b5845b34b2d7821ef4285e45568a5
Kirill <kirillmatuzko21@gmail.com>
2026-02-16 17:05:09 +0300

    add route

--- routes/web.php
+++ routes/web.php
@@ -2,6 +2,22 @@
 
 use Illuminate\Support\Facades\Route;
 
-Route::get('/', function () {
-    return view('home');
-});
+Route::get('/', 'App\Http\Controllers\MainController@home');
+
+Route::get('/about', 'App\Http\Controllers\MainController@about');
+
+Route::get('/review', 'App\Http\Controllers\MainController@review');
+
+Route::post('/review/check', 'App\Http\Controllers\MainController@reviewCheck');
+
+// Route::get('/about', function () {
+//     return view('about');
+// });
+
+// Route::get('/review', function () {
+//     return view('review');
+// });
+
+// Route::post('/review/check', function () {
+//     return view('review_check');
+// });
