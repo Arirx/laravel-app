680129b98db57945e81955b49d0168eac5903dc7
Kirill <kirillmatuzko21@gmail.com>
2026-02-20 20:27:57 +0300

    add upd maincontroller

--- app/Http/Controllers/MainController.php
+++ app/Http/Controllers/MainController.php
@@ -20,4 +20,13 @@ class MainController extends Controller
     {
         return view('review');
     }
+
+    public function reviewCheck(Request $request)
+    {
+        $valid = $request->validate([
+            'email' => 'required|min:4|max:100',
+            'subject' => 'required|min:4|max:100',
+            'message' => 'required|min:15|max:200',
+        ]);
+    }
 }
